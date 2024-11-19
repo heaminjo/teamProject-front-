edfa5af41cec2aecc5b4ad03626373295382dc21
heaminjo <157016835+heaminjo@users.noreply.github.com>
2024-11-19 22:56:54 +0900

    회원가입/로그인 에러 수정

--- src/api/AxiosApi.js
+++ src/api/AxiosApi.js
@@ -3,7 +3,7 @@ const KH_DOMAIN = "http://localhost:8089";
 
 const AxiosApi = {
   // 로그인
-  memberLogin: async (email, pwd) => {
+  login: async (email, pwd) => {
     const login = {
       email: email,
       pwd: pwd,
@@ -16,14 +16,14 @@ const AxiosApi = {
   },
 
   // 회원 가입
-  memberReg: async (email, pwd, alias, address) => {
+  signup: async (email, pwd, alias, address) => {
     const member = {
       email: email,
-      pwd: pwd,
       alias: alias,
       address: address,
+      pwd: pwd,
     };
-    return await axios.post(KH_DOMAIN + "/auth/join", member);
+    return await axios.post(KH_DOMAIN + "/auth/signup", member);
   },
   // 회원 가입 여부 확인
   memberRegCheck: async (email) => {
@@ -67,31 +67,6 @@ const AxiosApi = {
       content: content,
     };
     return await axios.post(KH_DOMAIN + `/api/comment/new`, comment);
-  },
-  // Todo 조회
-  todoList: async (userId) => {
-    return await axios.get(KH_DOMAIN + `/api/todo/list?userId=${userId}`);
-  },
-  // Todo 쓰기
-  todoInsert: async (userId, content) => {
-    const todo = {
-      userId: userId,
-      text: content,
-      checked: false,
-    };
-    return await axios.post(KH_DOMAIN + "/api/todo/new", todo);
-  },
-  // Todo 삭제
-  todoDelete: async (todoId) => {
-    return await axios.delete(KH_DOMAIN + `/api/todo/${todoId}`);
-  },
-  // Todo 수정
-  todoUpdate: async (todoId) => {
-    return await axios.put(KH_DOMAIN + `/api/todo/${todoId}`);
-  },
-  // 영화 목록 조회
-  movieList: async () => {
-    return await axios.get(KH_DOMAIN + "/api/movie/list");
   },
 };
 export default AxiosApi;
