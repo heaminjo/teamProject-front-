be5a85678ec8834d7796c6079363e50c2311b63b
heaminjo <157016835+heaminjo@users.noreply.github.com>
2024-11-20 23:30:05 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import GlobalStyle from "./util/GlobalStyle"; 
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import GlobalStyle from "./util/GlobalStyle";
+import Signup from "./pages/signup/Signup";
+import Login from "./pages/signup/Login";
+import Mainpage from "./pages/Mainpage";
+
 function App() {
   return (
-    
+    <>
+      <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Mainpage />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Join" element={<Signup />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
