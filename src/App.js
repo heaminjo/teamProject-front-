ea8ec08018aae9dd92cf7126e3863aaed69d3e6e
heaminjo <157016835+heaminjo@users.noreply.github.com>
2024-11-22 00:17:34 +0900

    헤더 완성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import GlobalStyle from "./util/GlobalStyle";
 import Signup from "./pages/signup/Signup";
 import Login from "./pages/signup/Login";
 import Mainpage from "./pages/Mainpage";
+import Layout from "./pages/Layout";
+import GlobalStyle from "./style/GlobalStyle";
 
 function App() {
   return (
@@ -12,9 +13,11 @@ function App() {
       <GlobalStyle />
       <Router>
         <Routes>
-          <Route path="/" element={<Mainpage />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/Join" element={<Signup />} />
+          <Route element={<Layout />}>
+            <Route path="/" element={<Mainpage />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/Signup" element={<Signup />} />
+          </Route>
         </Routes>
       </Router>
     </>
