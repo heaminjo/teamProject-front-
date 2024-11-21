39bd98b62e2d3ddb3026f1647631de4eb217b188
heaminjo <157016835+heaminjo@users.noreply.github.com>
2024-11-22 00:41:42 +0900

    Add files via upload

--- src/style/GlobalStyle.js
+++ src/style/GlobalStyle.js
@@ -10,5 +10,30 @@ const GlobalStyle = createGlobalStyle`
   li{
     list-style: none;
   }
+  main {
+    padding-top: 80px;
+    min-height: 60vh;
+  }
+  .container {
+    max-width: 1200px;
+    margin: 0 auto;
+    @media only screen and (max-width:1200px){ //
+      padding: 0 20px;
+    }
+  }
+  @media only screen and (max-width:768px) {
+    body {
+      font-size: 12px;
+    }
+    .container {
+      width: 90vw;
+      padding: 0;
+    }
+  }
+  @media only screen and (max-width: 480px) {
+    main {
+      padding-top: 40px;
+    }
+  }
 `;
 export default GlobalStyle;
