f02022367fd9d3a8b87464a1b49c5dae7d0dde24
heaminjo <157016835+heaminjo@users.noreply.github.com>
2024-11-27 23:46:30 +0900

    Add files via upload

---
+++ src/Component/Main/MainTop.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import backImg from "../../images/pexels-sanaan-3125171.jpg";
+const MainTopComp = styled.section`
+  background-image: url(${backImg});
+  background-size: cover;
+`;
+export default MainTopComp;
