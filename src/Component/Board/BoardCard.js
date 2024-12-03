d0e2fd01b4697eab8a9c6a0d9e79ccbdd06cba12
heaminjo <157016835+heaminjo@users.noreply.github.com>
2024-12-03 23:25:06 +0900

    좋아요 기능

--- src/Component/Board/BoardCard.js
+++ src/Component/Board/BoardCard.js
@@ -1,8 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import image from "../../images/Twitter.jpg";
 import basicPhoto from "../../images/Twitter.jpg";
+import falseHeart from "../../images/falseHeart.png";
+import trueHeart from "../../images/trueHeart.png";
+import BoardApi from "../../api/BoardApi";
+import HeartButton from "../../util/HeartButton";
+//하트아이콘 저작링크 <a href="https://www.flaticon.com/kr/free-icons/" title=" 아이콘"> 아이콘 제작자: Smashicons - Flaticon</a>
 const BoardCardComp = styled.div`
   display: flex;
   flex-direction: column;
@@ -66,6 +71,12 @@ const BoardCardComp = styled.div`
       align-items: end;
       font-size: 13px;
     }
+    .greatNum {
+      img {
+        width: 12px;
+        margin-right: 5px;
+      }
+    }
   }
 `;
 
@@ -88,13 +99,64 @@ const BoardCard = React.memo(
     const regDate = toDate.toISOString().split("T")[0];
     // console.log("렌더링 보드 : " + board.id);
 
+    const [heartNum, setHeartNum] = useState(board.greatNum); //좋아요 수
+    const [isHeart, setIsHeart] = useState(null); //하트아이콘
+    const isLogin = localStorage.getItem("isLogin");
+    const email = localStorage.getItem("email");
+
+    //하트 아이콘 랜더링
+    useEffect(() => {
+      if (email === null) {
+        setIsHeart(false);
+      } else {
+        isHeartCheck(board.id, email);
+      }
+    }, [isHeart]);
+
+    //사용자 좋아요 여부
+    const isHeartCheck = async (boardId, memberEmail) => {
+      const resp = await BoardApi.boardIsHeart(boardId, memberEmail);
+      console.log(resp.data);
+      if (resp.data === true) {
+        console.log("하트있음");
+        setIsHeart(true);
+      } else if (resp.data === false) {
+        console.log("하트없음");
+        setIsHeart(false);
+      } else if (resp.data === undefined) {
+        console.log("언디파인드임");
+      }
+    };
+    //좋아요 업데이트
+    const postHeart = async (boardId, memberEmail) => {
+      const rsp = await BoardApi.boardHeart(boardId, memberEmail);
+
+      console.log("현재 하트 상태" + isHeart);
+      if (isHeart === true) {
+        setIsHeart(false);
+        setHeartNum(heartNum - 1);
+      } else {
+        setIsHeart(true);
+        setHeartNum(heartNum + 1);
+      }
+    };
+
+    //좋아요 클릭
+    const onClickHeart = () => {
+      if (isLogin !== "TRUE") {
+        alert("로그인이 필요한 서비스 입니다.");
+        navigate("/login");
+      } else {
+        postHeart(board.id, email);
+      }
+    };
     return (
       <>
         <BoardCardComp
           className="mapBox"
-          onClick={() => {
-            navigate(`/board/post/${board.id}`);
-          }}
+          // onClick={() => {
+          //   navigate(`/board/post/${board.id}`);
+          // }}
         >
           <div className="postTop">
             <div className="memberProfile">
@@ -124,7 +186,8 @@ const BoardCard = React.memo(
                 <p>조회수 {board.views}</p>
               </div>
               <div className="greatNum">
-                <p>좋아요 {board.greatNum}</p>
+                <HeartButton isHeart={isHeart} onClickHeart={onClickHeart} />
+                {heartNum}
               </div>
             </div>
           </div>
